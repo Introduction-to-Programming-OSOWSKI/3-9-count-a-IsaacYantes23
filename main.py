ba54c8fe811a91824cebc3828942588422a0181c
IsaacYantes23 <817296@mystma.org>
2021-02-03 02:18:08 +0000

    counta try 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def countA(w):
+    count = w["a"]
+    
+    for i in range(0,len(w)):
+        if w[i]:
+            return count
+        
+    return False
+    
+print(countA("cat"))
